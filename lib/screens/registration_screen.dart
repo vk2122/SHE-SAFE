4880a19e97c2f65aafc2b6ae3db62dab491c93c8
Varun Khachane <varunkhachane31@gmail.com>
2022-06-10 22:18:46 +0530

    bugs fixed

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_new
 
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_authentication_demo/model/user_model.dart';
